6c018982c9e862cb1506003636ff81d00c643b49
wechselwesen <wechselwesen@gmail.com>
2024-04-08 16:26:00 +0200

    Pageloader
    pageloader implementiert.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './style/App.css';
 import './style/GlobalVars.css';
+import Pageloader from './components/Pageloader/Pageloader';
 
 function App() {
   return (
     <div className="App">
-      <h1>FilmRausch</h1>
-      <p>hier kommt die Beschreibung</p>
+ <Pageloader />
     </div>
   );
 }
