6bde6871b00f41a58f9852c5865af88f0484c8a4
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-08 12:36:03 +0200

    Navbar ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './style/App.css';
 import './style/GlobalVars.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>FilmRausch</h1>
       <p>hier kommt die Beschreibung</p>
     </div>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faFilm, faTv, faSearch, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles["navbar-container"]}>
+        <div className={styles["navbar-brand"]}>
+          <img src="logo.png" alt="FilmRausch Logo" className={styles["navbar-logo"]}/>
+          <span className={styles["navbar-title"]}>FilmRausch</span>
+        </div>
+        <div className={styles.linkContainer}>
+          <ul className={styles["navbar-menu"]}>
+            <li className={styles["navbar-item"]}>
+              <a href="#" className={styles["navbar-link"]}>
+              <FontAwesomeIcon icon={faHome} /> Startseite
+              </a>
+            </li>
+            <li className={styles["navbar-item"]}>
+              <a href="#" className={styles["navbar-link"]}>
+              <FontAwesomeIcon icon={faNewspaper} /> Neuheiten
+              </a>
+            </li>
+            <li className={styles["navbar-item"]}>
+              <a href="#" className={styles["navbar-link"]}>
+               <FontAwesomeIcon icon={faFilm} /> Filme
+              </a>
+            </li>
+            <li className={styles["navbar-item"]}>
+              <a href="#" className={styles["navbar-link"]}>
+              <FontAwesomeIcon icon={faTv} /> Serien
+              </a>
+            </li>
+            <li className={styles["navbar-item"]}>
+              <a href="#" className={styles["navbar-link"]}>
+              <FontAwesomeIcon icon={faSearch} /> Suche
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
