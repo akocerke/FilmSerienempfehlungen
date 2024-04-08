cd47e781430b1f97e1621bca35352c16b0d099f6
gabriel <gabrielmasquediez@gmail.com>
2024-04-08 16:27:29 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
-import './style/App.css';
-import './style/GlobalVars.css';
+import "./style/App.css";
+import "./style/GlobalVars.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>FilmRausch</h1>
-      <p>hier kommt die Beschreibung</p>
+      <p>hier kommt die Beschreibung </p>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="#"> Start Seite </a>
+          </li>
+          <li>
+            <a href="#"> Filme </a>
+          </li>
+          <li>
+            <a href="#"> Serien </a>
+          </li>
+          <li>
+            <a href="#"> Serien </a>
+          </li>
+          <li>
+            <a href="#"> Serien </a>
+          </li>
+          <li>
+            <a href="#"> Serien </a>
+          </li>
+          <li>
+            <a href="#"> Serien </a>
+          </li>
+          <li>
+            <a href="#"> Serien </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
