338fd8b0a39c1755c2a623ae3d9b276248af4477
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-09 10:43:33 +0200

    Pageloader auskommentiert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './style/App.css';
 import './style/GlobalVars.css';
-import Pageloader from './components/Pageloader/Pageloader';
+// import Pageloader from './components/Pageloader/Pageloader';
 import Navbar from './components/Navbar/Navbar';
 import Footer from "./components/Footer/Footer";
 
@@ -8,7 +8,7 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Pageloader />
+      {/* <Pageloader /> */}
       <Navbar />
       <Footer />
     </div>
