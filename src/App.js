25dc3a521951768089bc0a82b0ed05a23eb753db
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-09 14:58:54 +0200

    Pageloader Implementiert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './style/App.css';
-import './style/GlobalVars.css';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Startseite from "./Pages/Startseite/Startseite";
 import Filme from "./Pages/Filme/Filme";
@@ -12,29 +11,43 @@ import Impressum from "./Pages/Impressum/Impressum";
 import Footer from "./components/Footer/Footer";
 import Navbar2 from "./components/Navbar2/Navbar2";
 import Header from './components/Header/Header';
-
+import Pageloader from './components/Pageloader/Pageloader';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulieren einer Ladezeit
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4800); // Zum Beispiel 2000ms = 2 Sekunden
+  }, []);
+
   return (
     <>
-
       <BrowserRouter>
-      <Header />
-      <Navbar2 />
-      <Footer />
-        <Routes>
-          <Route path="/" element={<Startseite />} exact />
-          <Route path="/filme" element={<Filme />} />
-          <Route path="/serien" element={<Serien />} />
-          <Route path="/suche" element={<Suche />} />
-          <Route path="*" element={<Error />} />
-          <Route path="/impressum" element={<Impressum />} />
-          <Route path="/favoriten" element={<Favoriten />} />
-        </Routes>
+        {isLoading ? (
+          <Pageloader />
+        ) : (
+          <>
+            <Header />
+            <Navbar2 />
+            <Footer />
+            <Routes>
+              <Route path="/" element={<Startseite />} exact />
+              <Route path="/filme" element={<Filme />} />
+              <Route path="/serien" element={<Serien />} />
+              <Route path="/suche" element={<Suche />} />
+              <Route path="/filmseite/:id" element={<Filmseite />} />
+              <Route path="/favoriten" element={<Favoriten />} />
+              <Route path="/impressum" element={<Impressum />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </>
+        )}
       </BrowserRouter>
     </>
   );
 };
- 
 
 export default App;
