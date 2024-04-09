b61aca6f446d0fd48d0a5f8b69e215b646bcc04c
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-09 16:05:01 +0200

    Navbar2 Header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () =>  {
     return (
         <>
         <div className={styles["container-fluid"]}>
-            <div className="logo">
-                    <img src="logo.png" alt="FilmRausch Logo" class={styles["logo"]}/>
+            <div class={styles["logo"]}>
+                    <img src="logo.png" alt="FilmRausch Logo" class={styles["logoSize"]}/>
             </div>
         </div>
         </>
