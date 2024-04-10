218d022aa172262c05636b00c9b6c2793d1dea60
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-10 15:20:00 +0200

    content hight update

--- src/components/Favoriten.js
+++ src/components/Favoriten.js
@@ -8,7 +8,7 @@ const AddFavourites = () => {
 				width='1em'
 				height='1em'
 				viewBox='0 0 16 16'
-				class='bi bi-heart-fill'
+				// class='bi bi-heart-fill'
 				fill='red'
 				xmlns='http://www.w3.org/2000/svg'
 			>
