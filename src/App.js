725e00e6c8605aa194e86a4906f28a68225da350
techmocker <kevin.losem@docc.techstarter.de>
2024-04-10 14:16:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Startseite from "./Pages/Startseite/Startseite";
 import Filme from "./Pages/Filme/Filme";
 import Serien from "./Pages/Serien/Serien";
 import Suche from "./Pages/Suche/Suche";
+import Filmseite from "./Pages/Filmseite/Filmseite";
 import Favoriten from "./Pages/Favoriten/Favoriten";
 import Error from "./Pages/Error/Error";
 import Impressum from "./Pages/Impressum/Impressum";
@@ -11,13 +12,12 @@ import Footer from "./components/Footer/Footer";
 import Navbar2 from "./components/Navbar2/Navbar2";
 import Header from './components/Header/Header';
 import Pageloader from './components/Pageloader/Pageloader';
-import Neuheiten from './Pages/Neuheiten/Neuheiten';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simulieren einer Ladezeit
+    // Simulieren einer Ladezeit bla bla
     setTimeout(() => {
       setIsLoading(false);
     }, 4800); // Zum Beispiel 2000ms = 2 Sekunden
@@ -26,19 +26,25 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Header />
-      <Navbar2 />
-      <Footer />
-        <Routes>
-          <Route path="/" element={<Startseite />} exact />
-          <Route path="/filme" element={<Filme />} />
-          <Route path="/serien" element={<Serien />} />
-          <Route path="/suche" element={<Suche />} />
-          <Route path="*" element={<Error />} />
-          <Route path="/impressum" element={<Impressum />} />
-          <Route path="/favoriten" element={<Favoriten />} />
-          <Route path="/neuheiten" element={<Neuheiten />} />
-        </Routes>
+        {isLoading ? (
+          <Pageloader />
+        ) : (
+          <>
+            <Header />
+            <Navbar2 />
+            <Footer />
+            <Routes>
+              <Route path="/" element={<Startseite />} exact />
+              <Route path="/filme" element={<Filme />} />
+              <Route path="/serien" element={<Serien />} />
+              <Route path="/suche" element={<Suche />} />
+              <Route path="/filmseite/:id" element={<Filmseite />} />
+              <Route path="/favoriten" element={<Favoriten />} />
+              <Route path="/impressum" element={<Impressum />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </>
+        )}
       </BrowserRouter>
     </>
   );
