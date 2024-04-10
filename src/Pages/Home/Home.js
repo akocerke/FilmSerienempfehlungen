d6216e9055df07b1d85034c6a43f0bef2d8dbe72
gabriel <gabrielmasquediez@gmail.com>
2024-04-10 09:55:07 +0200

    Startseite

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,9 +3,9 @@ import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <div className={styles["fondo"]}>
-      <h1>Willkommen</h1>
-      <h2>
+    <div className={styles.popcorncss}>
+      <h1 className={styles["popkorn"]}>Willkommen</h1>
+      <h2 className={styles["popkorn1"]}>
         Millionen von Filmen, Serien und Menschen zu entdecken. Jetzt erkunden.
       </h2>
     </div>
