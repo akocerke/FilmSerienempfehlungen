6c0ae442e2a09557769b18544a1b5993d8dc7ef0
gabriel <gabrielmasquediez@gmail.com>
2024-04-11 12:17:56 +0200

    Poster

--- src/Pages/Filme/PopularMovies.js
+++ src/Pages/Filme/PopularMovies.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { fetchMovies } from "../../../src/apiService";
-
+import Content from "../../components/Content/Content";
 const PopularMovies = () => {
-     const [movies, setMovies] = useState([]);
-        useEffect(() => { 
-            const fetchMoviesData = async () => {
-                 const moviesData = await fetchMovies();
-                  setMovies(moviesData); };
-                   fetchMoviesData(); }, []); 
-                   
-                   return ( <div> <h1>Populäre Filme</h1> 
-                   {movies.map((movie) => ( 
-                   <div key={movie.id}> 
-                   <h2>{movie.title}</h2> 
-                   <p>{movie.overview}</p> 
-                   </div> ))} </div> 
-                   );
-                };
-                   
-                   
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchMoviesData = async () => {
+      const moviesData = await fetchMovies();
+      setMovies(moviesData);
+    };
+    fetchMoviesData();
+  }, []);
+
+  return (
+    <Content>
+      <div>
+        <h1>Populäre Filme</h1>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img
+              src={`https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <p>Popularidad: {movie.popularity}%</p>
+            <p>{movie.overview}</p>
+          </div>
+        ))}{" "}
+      </div>
+    </Content>
+  );
+};
+
 export default PopularMovies;
--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 const API_KEY = "1ad397f85b6fe90915ecb92e15c0a3cc";
-const apiService = axios.create({ baseURL: "https://api.themoviedb.org/3/",});
+const apiService = axios.create({ baseURL: "https://api.themoviedb.org/3/" });
 
-export const fetchMovies = async () => { try { const response = await apiService.get("movie/popular", { params: { api_key: API_KEY, }, });
-return response.data.results; } catch (error) { console.error("Error fetching movies", error); return []; }}
+export const fetchMovies = async () => {
+  try {
+    const response = await apiService.get("movie/popular", {
+      params: { api_key: API_KEY },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching movies", error);
+    return [];
+  }
+};
