80c32c6f1ced3e26073b8a0d22ae8b15d8973499
gabriel <gabrielmasquediez@gmail.com>
2024-04-11 08:47:44 +0200

    copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,38 @@
 import { Link } from "react-router-dom";
-import React from 'react';
-import './Footer.module.css'; // Stile hier importieren
+import React from "react";
+import "./Footer.module.css"; // Stile hier importieren
+import Content from "../Content/Content";
 
 function Footer() {
+  const fechaActual = new Date().getFullYear();
+
   return (
-    <footer className="Footer" style={{ position: "fixed", bottom: 0, width: "100%" }}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">StartSeite</Link>
-          </li>
-          <li>
-            <Link to="/Filme">Filme</Link>
-          </li>
-          <li>
-            <Link to="/Serien">Serien</Link>
-          </li>
-          <li>
-            <Link to="/Impressum">Impressum</Link>
-          </li>
-        </ul>
-      </nav>
-    </footer>
+    <Content>
+      <footer
+        className="Footer"
+        style={{ position: "fixed", bottom: 0, width: "100%" }}
+      >
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">StartSeite</Link>
+            </li>
+            <li>
+              <Link to="/Filme">Filme</Link>
+            </li>
+            <li>
+              <p>&copy; {fechaActual} Filmrausch</p>
+            </li>
+            <li>
+              <Link to="/Serien">Serien</Link>
+            </li>
+            <li>
+              <Link to="/Impressum">Impressum</Link>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </Content>
   );
 }
 
