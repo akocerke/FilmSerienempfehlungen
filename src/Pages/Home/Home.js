4c21ef346b979da8c7805f36b9d75b55e11a79bd
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-12 12:28:49 +0200

    startseite css ready

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,8 +7,11 @@ function Home() {
       <div className={styles.popcorncss2}>
         <h1 className={styles["popkorn"]}>Willkommen im Filmrausch!</h1>
         <h2 className={styles["popkorn1"]}>
-        Dein Portal zur ultimativen Film- und Seriendatenbank. Entdecke eine schier endlose Auswahl an Titeln und lass dich von der Vielfalt des Films verzaubern. Bereit, deine nächste Lieblingsproduktion zu finden?
-        </h2>
+        Dein Portal zur ultimativen Film- und Seriendatenbank.</h2>
+
+        <h2 className={styles["popkorn1"]}>Bereit, deine nächste Lieblingsproduktion zu finden?</h2> 
+        
+        
       </div>
     </div>
   );
