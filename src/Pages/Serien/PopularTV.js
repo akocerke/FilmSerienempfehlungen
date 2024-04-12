8fce7a76eee0435841ace8305afc0349dfa13717
techmocker <kevin.losem@docc.techstarter.de>
2024-04-12 10:58:25 +0200

    Design Update

--- src/Pages/Serien/PopularTV.js
+++ src/Pages/Serien/PopularTV.js
@@ -3,34 +3,52 @@ import { fetchMovies } from "../../../src/apiServiceTV";
 import Content from "../../components/Content/Content";
 import styles from "./Serien.module.css"
 
+const PopularTV = () => { 
+   const [movies, setMovies] = useState([]);
+   useEffect(() => {
+     const fetchMoviesData = async () => {
+       const moviesData = await fetchMovies();
+       setMovies(moviesData);
+     };
+     fetchMoviesData();
+   }, []);
+ 
+   // Funktion zur Konvertierung des Datums in das deutsche Format
+   const formatDateGerman = (dateString) => {
+     const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+     return new Date(dateString).toLocaleDateString('de-DE', options);
+   };
+ 
+   return (
+     <Content>
+       <div className={styles.ontop}>
+         <h1 className={styles.ueberschrift}>Populäre Serien</h1>
+       </div>
+       <hr className={styles.new5} />
+       <div className={styles.gridContainer}>
+         {movies.map((movie) => (
+           <div className={styles.gridItemContent} key={movie.id}>
+             <div className={styles.gridItem}>
+               <img
+                 src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+                 alt={movie.name}
+               />
+             </div>
+             <div className={styles.beschreibung}>
+               <div className={styles.titleContainer}>
+                 <h5 className={styles.title}>{movie.name}</h5>
+               </div>
+               <div className={styles.releasedateContainer}>
+                 {/* Veröffentlichungsdatum im deutschen Format anzeigen */}
+                 <p className={styles.releasedate}>{formatDateGerman(movie.first_air_date)}</p>
+               </div>
+             </div>
+           </div>
+         ))}
+       </div>
+     </Content>
+   );
+ };
+ 
 
-const PopularTV = () => {
-     const [movies, setMovies] = useState([]);
-        useEffect(() => { 
-            const fetchMoviesData = async () => {
-                 const moviesData = await fetchMovies();
-                  setMovies(moviesData); };
-                   fetchMoviesData(); }, []); 
-                   
-                   return ( 
-                   <Content>
-                    <div>
-                    <h1 className={styles.ueberschrift}>Populäre Serien</h1> 
-                    </div>
-
-                    <div className={styles.gridContainer}>  {/* Kompletter Container */}
-                    
-                   {movies.map((movie) => ( 
-                    <div className={styles.gridItemContent}>
-                   <div key={movie.id} className={styles.gridItem}> 
-                   <img src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}alt={movie.name} className={styles.posterimg}/>
-                   <h2 className={styles.title}>{movie.name}</h2> 
-                   <h3 className={styles.releasedate}>{movie.first_air_date}</h3>
-                   </div>
-                   </div> ))} </div> 
-                   </Content>
-                   );
-                };
-                   
-                   
 export default PopularTV;
