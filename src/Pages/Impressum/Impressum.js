b8039ca33664c70ce6272af9ed24f189cf400b6c
gabriel <gabrielmasquediez@gmail.com>
2024-04-12 09:22:24 +0200

    impressum

--- src/Pages/Impressum/Impressum.js
+++ src/Pages/Impressum/Impressum.js
@@ -1,11 +1,133 @@
 import React from "react";
+import Content from "../../components/Content/Content";
+import styles from "../../Pages/Impressum/Impressum.module.css";
+
+const style = {
+  primaryColor: {
+    color: "var(--primary-color)",
+  },
+};
 
 const Impressum = () => {
-    return (
-      <div>
+  return (
+    <Content>
+      <div className={styles.titel}>
+        <h1>Impressum</h1>
       </div>
-    );
-  }
-  
-  export default Impressum;
-  
+      <div className={styles.impressum}>
+        <div className={styles.Angaben}>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Angaben gemäß § 5 TMG</strong>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Firma</strong>{" "}
+            <span style={style.primaryColor}>Filmrausch GmbH</span>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Adresse</strong>{" "}
+            <span style={style.primaryColor}>
+              {" "}
+              Musterstraße 123, 12345 Musterstadt
+            </span>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Telefon</strong>{" "}
+            <span style={style.primaryColor}>+49 123 4567890</span>
+          </p>
+
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Adresse</strong>{" "}
+            <span style={style.primaryColor}>
+              Musterstraße 123, 12345 Musterstadt
+            </span>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>E-Mail</strong>{" "}
+            <span style={style.primaryColor}>
+              info-filmrausch@maxmonster.de
+            </span>
+          </p>
+        </div>
+        <div ClassName={styles.Handelsregister}>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Handelsregister</strong>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Registergericht</strong>{" "}
+            <span style={style.primaryColor}>Amtsgericht Musterstadt</span>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>Registernummer</strong>{" "}
+            <span style={style.primaryColor}> HRB 12345</span>
+          </p>
+
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>
+              Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz
+            </strong>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>E-Mail</strong>{" "}
+            <span style={style.primaryColor}>
+              info-filmrausch@maxmonster.de
+            </span>
+          </p>
+        </div>
+        <div ClassName={styles.Urheberrecht}>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>
+              Urheberrecht und Marken
+            </strong>
+          </p>
+          <p>
+            <span style={style.primaryColor}>
+              Alle auf dieser Website verwendeten Marken und Logos sind Eigentum
+              ihrer jeweiligen Inhaber.{" "}
+            </span>
+          </p>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>
+              Umsatzsteuer-Identifikationsnummer
+            </strong>
+            <span style={style.primaryColor}>DE123456789</span>
+          </p>
+
+          <p>
+            {" "}
+            <strong style={{ fontSize: "1.2em" }}>Aufsichtsbehörde</strong>
+          </p>
+          <p>
+            {" "}
+            <span style={style.primaryColor}>
+              Regulierungsbehörde Musterland
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className={styles.UnterImpressum}>
+        <div className={styles.UnterImpressum1}>
+          <p>
+            <strong style={{ fontSize: "1.2em" }}>
+              Plattform der EU-Kommission zur Online-Streitbeilegung
+            </strong>
+          </p>
+          <p>
+            {" "}
+            <span style={style.primaryColor}>
+              {" "}
+              Die Europäische Kommission stellt eine Plattform zur
+              Online-Streitbeilegung (OS) bereit. Die Plattform finden Sie
+              unter:
+              <a href="https://ec.europa.eu/consumers/odr">
+                Link zur OS-Plattform
+              </a>
+            </span>
+            .
+          </p>
+        </div>
+      </div>
+    </Content>
+  );
+};
+
+export default Impressum;
