07dbe829123b5f4bccfd267f1d407b57cba2d769
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-12 12:04:21 +0200

    css update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,17 @@
 import React from "react";
 import styles from "./Home.module.css";
-import Content from '../../components/Content/Content';
 
 function Home() {
   return (
-    <Content>
-      <div className={styles.popcorncss}>
-      <h1 className={styles["popkorn"]}>Willkommen</h1>
-      <h2 className={styles["popkorn1"]}>
-        Millionen von Filmen, Serien und Menschen zu entdecken. Jetzt erkunden.
-      </h2>
+    <div className={styles.popcorncss}>
+      <div className={styles.popcorncss2}>
+        <h1 className={styles["popkorn"]}>Willkommen im Filmrausch!</h1>
+        <h2 className={styles["popkorn1"]}>
+          Millionen von Filmen, Serien und Menschen zu entdecken. Jetzt
+          erkunden.
+        </h2>
+      </div>
     </div>
-    </Content>
-    
   );
 }
 
