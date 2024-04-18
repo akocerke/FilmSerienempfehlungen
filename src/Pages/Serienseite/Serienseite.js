3e4c882c03c028696631ba55811aec2b42d8933d
techmocker <kevin.losem@docc.techstarter.de>
2024-04-18 15:31:52 +0200

    Updated

--- src/Pages/Serienseite/Serienseite.js
+++ src/Pages/Serienseite/Serienseite.js
@@ -1,51 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { fetchSeriesDetails } from "../../apiService";
+import { fetchSeriesDetails } from "../../../src/apiService";
 import styles from "./Serienseite.module.css";
+import Content from "../../components/Content/Content";
+import ActorCarouselTV from "./ActorCarouselTV";
+
+const formatDateGerman = (dateString) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString('de-DE', options);
+};
+
+const formatPercentage = (rating) => {
+  return `${Math.round(rating * 10)}%`; 
+};
+
+
+
 
 const Serienseite = () => {
   const { id } = useParams();
-  const [content, setContent] = useState(null); 
-  const [isMovie, setIsMovie] = useState(true); 
-  const [isLoading, setIsLoading] = useState(true); // Zustand um den Ladezustand zu verfolgen
+  const [movie, setMovie] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let contentData;
-        if (!isMovie) {
-          contentData = await fetchSeriesDetails(id);
-          setContent(contentData);
-        }
-      } catch (error) {
-        console.error("Error fetching content details: ", error);
-      } finally {
-        setIsLoading(false); // Setze isLoading auf false, wenn die Daten geladen sind
-      }
+    const fetchSeriesData = async () => {
+      const movieData = await fetchSeriesDetails(id);
+      setMovie(movieData);
     };
-    fetchData();
-  }, [id, isMovie]);
+    fetchSeriesData();
+  }, [id]);
 
-  useEffect(() => {
-    setIsMovie(window.location.pathname.includes("filme"));
-  }, [window.location.pathname]);
-
-  // Prüfe, ob die Daten noch geladen werden
-  if (isLoading || !content) {
+  if (!movie) {
     return <div className={styles.container}>Am Laden...</div>;
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.moviedetails}>
-        <h2 className={styles.title}>{content.title}</h2>
-        <img className={styles.poster} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${content.poster_path}`} alt={content.title} />
-        <p className={styles.overview}>{content.overview}</p>
-        <p className={styles.releasedate}>Release Date: {content.first_air_date}</p>
-        <p className={styles.rating}>Rating: {content.vote_average}</p>
-        <Link to="/serien" className={styles.backlink}>Zurück zu Serien</Link>
+    <Content>
+      <div className={styles.container}>
+        <div className={styles.moviedetails}>
+          <h2 className={styles.title}>{movie.title}</h2>
+          <img className={styles.poster} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} alt={movie.title} />
+          <h2 className={styles.tagline}>{movie.tagline}</h2>
+          <h3 className={styles.status}>{movie.status}</h3>
+          <p className={styles.overview}>{movie.overview ? `$${movie.overview}` : "Keine Beschreibung verfügbar"}</p>
+
+              <ActorCarouselTV seriesId={id} />
+          <div className={styles.info}>
+            <p className={styles.releasedate}>Release Date: {formatDateGerman(movie.first_air_date)}</p>
+            <p className={styles.rating}>
+                Rating: <span className={`${styles.circle} ${movie.vote_average >= 5 ? styles.green : styles.red}`}>{formatPercentage(movie.vote_average)}</span>
+            </p>
+            <p className={styles.budget}>Budget: {movie.Budget ? `$${movie.Budget}` : "Unbekannt"}</p>
+            <p className={styles.studio}>Studio: {movie.production_companies && movie.production_companies.map(production_companies => production_companies.name).join(', ')}</p>
+            <p className={styles.genres}>Genres: {movie.genres && movie.genres.map(genre => genre.name).join(', ')}</p>
+          </div>
+          <Link to="/serien" className={styles.backlink}>Zurück zu Serien</Link>
+        </div>
       </div>
-    </div>
+    </Content>
   );
 };
 
