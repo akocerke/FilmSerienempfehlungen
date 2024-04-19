b9dc946c431c356fb2ed9e69fbb9683917cb91d3
techmocker <kevin.losem@docc.techstarter.de>
2024-04-19 13:52:22 +0200

    update

--- src/Pages/Favoriten/Favoriten.js
+++ src/Pages/Favoriten/Favoriten.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getFavoritesByUserId } from "../../apiUser";
 
-const Favoriten = () => {
-    return (
-      <div>
-      </div>
-    );
-  }
-  
-  export default Favoriten;
-  
+
+const Favoriten = ({ userId }) => {
+  const [favoriten, setFavoriten] = useState([]);
+
+  useEffect(() => {
+    const fetchFavoriten = async () => {
+      try {
+        const favoritenData = await getFavoritesByUserId(userId);
+        setFavoriten(favoritenData.movieIds);
+      } catch (error) {
+        console.error("Fehler beim Abrufen der Favoriten:", error);
+      }
+    };
+    fetchFavoriten();
+  }, [userId]);
+
+  return (
+    <div>
+      <h2>Deine Favoriten</h2>
+      <ul>
+        {favoriten.map((movieId) => (
+          <li key={movieId}>{movieId}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Favoriten;
