a1f6d9936b782b2af952d72fb3e8913f44e541d5
gabriel <gabrielmasquediez@gmail.com>
2024-04-23 12:07:51 +0200

    suche btn

--- src/Pages/Filmseite/Filmseite.js
+++ src/Pages/Filmseite/Filmseite.js
@@ -6,16 +6,13 @@ import Content from "../../components/Content/Content";
 import ActorCarousel from "./ActorCarousel";
 
 const formatDateGerman = (dateString) => {
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return new Date(dateString).toLocaleDateString('de-DE', options);
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  return new Date(dateString).toLocaleDateString("de-DE", options);
 };
 
 const formatPercentage = (rating) => {
-  return `${Math.round(rating * 10)}%`; 
+  return `${Math.round(rating * 10)}%`;
 };
-
-
-
 
 const Filmseite = () => {
   const { id } = useParams();
@@ -38,21 +35,57 @@ const Filmseite = () => {
       <div className={styles.container}>
         <div className={styles.moviedetails}>
           <h2 className={styles.title}>{movie.title}</h2>
-          <img className={styles.poster} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} alt={movie.title} />
+          <img
+            className={styles.poster}
+            src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+            alt={movie.title}
+          />
           <h2 className={styles.tagline}>{movie.tagline}</h2>
           <h3 className={styles.status}>{movie.status}</h3>
-          <p className={styles.overview}>{movie.overview ? `$${movie.overview}` : "Keine Beschreibung verfügbar"}</p>
-              <ActorCarousel movieId={id} />
+          <p className={styles.overview}>
+            {movie.overview
+              ? `$${movie.overview}`
+              : "Keine Beschreibung verfügbar"}
+          </p>
+          <ActorCarousel movieId={id} />
           <div className={styles.info}>
-            <p className={styles.releasedate}>Release Date: {formatDateGerman(movie.release_date)}</p>
-            <p className={styles.rating}>
-                Rating: <span className={`${styles.circle} ${movie.vote_average >= 5 ? styles.green : styles.red}`}>{formatPercentage(movie.vote_average)}</span>
+            <p className={styles.releasedate}>
+              Release Date: {formatDateGerman(movie.release_date)}
             </p>
-            <p className={styles.budget}>Budget: {movie.budget ? `$${movie.budget}` : "Unbekannt"}</p>
-            <p className={styles.studio}>Studio: {movie.production_companies && movie.production_companies.map(production_companies => production_companies.name).join(', ')}</p>
-            <p className={styles.genres}>Genres: {movie.genres && movie.genres.map(genre => genre.name).join(', ')}</p>
+            <p className={styles.rating}>
+              Rating:{" "}
+              <span
+                className={`${styles.circle} ${
+                  movie.vote_average >= 5 ? styles.green : styles.red
+                }`}
+              >
+                {formatPercentage(movie.vote_average)}
+              </span>
+            </p>
+            <p className={styles.budget}>
+              Budget: {movie.budget ? `$${movie.budget}` : "Unbekannt"}
+            </p>
+            <p className={styles.studio}>
+              Studio:{" "}
+              {movie.production_companies &&
+                movie.production_companies
+                  .map((production_companies) => production_companies.name)
+                  .join(", ")}
+            </p>
+            <p className={styles.genres}>
+              Genres:{" "}
+              {movie.genres &&
+                movie.genres.map((genre) => genre.name).join(", ")}
+            </p>
           </div>
-          <Link to="/filme" className={styles.backlink}>Zurück zu Filme</Link>
+          <div className={styles.backlinkContainer}>
+            <Link to="/filme" className={styles.backlink}>
+              Zurück zu Filme
+            </Link>
+            <Link to="/suche" className={styles.backlink}>
+              Suche mehr Filme
+            </Link>
+          </div>
         </div>
       </div>
     </Content>
--- src/Pages/Serienseite/Serienseite.js
+++ src/Pages/Serienseite/Serienseite.js
@@ -6,16 +6,13 @@ import Content from "../../components/Content/Content";
 import ActorCarouselTV from "./ActorCarouselTV";
 
 const formatDateGerman = (dateString) => {
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return new Date(dateString).toLocaleDateString('de-DE', options);
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  return new Date(dateString).toLocaleDateString("de-DE", options);
 };
 
 const formatPercentage = (rating) => {
-  return `${Math.round(rating * 10)}%`; 
+  return `${Math.round(rating * 10)}%`;
 };
-
-
-
 
 const Serienseite = () => {
   const { id } = useParams();
@@ -38,21 +35,57 @@ const Serienseite = () => {
       <div className={styles.container}>
         <div className={styles.moviedetails}>
           <h2 className={styles.title}>{movie.title}</h2>
-          <img className={styles.poster} src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} alt={movie.title} />
+          <img
+            className={styles.poster}
+            src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+            alt={movie.title}
+          />
           <h2 className={styles.tagline}>{movie.tagline}</h2>
           <h3 className={styles.status}>{movie.status}</h3>
-          <p className={styles.overview}>{movie.overview ? `$${movie.overview}` : "Keine Beschreibung verfügbar"}</p>
-              <ActorCarouselTV seriesId={id} />
+          <p className={styles.overview}>
+            {movie.overview
+              ? `$${movie.overview}`
+              : "Keine Beschreibung verfügbar"}
+          </p>
+          <ActorCarouselTV seriesId={id} />
           <div className={styles.info}>
-            <p className={styles.releasedate}>Release Date: {formatDateGerman(movie.first_air_date)}</p>
-            <p className={styles.rating}>
-                Rating: <span className={`${styles.circle} ${movie.vote_average >= 5 ? styles.green : styles.red}`}>{formatPercentage(movie.vote_average)}</span>
+            <p className={styles.releasedate}>
+              Release Date: {formatDateGerman(movie.first_air_date)}
             </p>
-            <p className={styles.budget}>Budget: {movie.budget ? `$${movie.budget}` : "Unbekannt"}</p>
-            <p className={styles.studio}>Studio: {movie.production_companies && movie.production_companies.map(production_companies => production_companies.name).join(', ')}</p>
-            <p className={styles.genres}>Genres: {movie.genres && movie.genres.map(genre => genre.name).join(', ')}</p>
+            <p className={styles.rating}>
+              Rating:{" "}
+              <span
+                className={`${styles.circle} ${
+                  movie.vote_average >= 5 ? styles.green : styles.red
+                }`}
+              >
+                {formatPercentage(movie.vote_average)}
+              </span>
+            </p>
+            <p className={styles.budget}>
+              Budget: {movie.budget ? `$${movie.budget}` : "Unbekannt"}
+            </p>
+            <p className={styles.studio}>
+              Studio:{" "}
+              {movie.production_companies &&
+                movie.production_companies
+                  .map((production_companies) => production_companies.name)
+                  .join(", ")}
+            </p>
+            <p className={styles.genres}>
+              Genres:{" "}
+              {movie.genres &&
+                movie.genres.map((genre) => genre.name).join(", ")}
+            </p>
           </div>
-          <Link to="/serien" className={styles.backlink}>Zurück zu Serien</Link>
+          <div className={styles.backlinkContainer}>
+            <Link to="/serien" className={styles.backlink}>
+              Zurück zu Serien
+            </Link>
+            <Link to="/suche" className={styles.backlink}>
+              Suche mehr Serien
+            </Link>
+          </div>
         </div>
       </div>
     </Content>
